6fd9e4830befc15a30ceefdbd69fca1402588b75
Martin Vajnar <martin.vajnar@gmail.com>
2025-11-09 17:14:43 +0100

    Use INADDR_ANY on bind() instead of hard-coded 127.0.0.1

--- linux+pi/pty2udp.c
+++ linux+pi/pty2udp.c
@@ -89,7 +89,7 @@ void setupUdpConnection(void) {
 	struct sockaddr_in daddr;
 	memset(&daddr, 0, sizeof(struct sockaddr_in));
 	daddr.sin_family = AF_INET;
-	daddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	daddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	daddr.sin_port = htons(9877);
 
 	bind(udp_socket, (struct sockaddr *)&daddr, sizeof(struct sockaddr_in));
