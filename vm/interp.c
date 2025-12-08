6db037101c8cf784c348882119fd2b51a796532c
Martin Vajnar <martin.vajnar@gmail.com>
2025-12-08 16:06:22 +0100

    Make VM more responsive

--- vm/interp.c
+++ vm/interp.c
@@ -1394,7 +1394,7 @@ void vmLoop() {
 				cocubeSensorUpdate();
 			#endif
 			handleMicosecondClockWrap();
-			count = 95; // must be under 30 when building on mbed to avoid serial errors
+			count = 0; // must be under 30 when building on mbed to avoid serial errors
 		} else if ((count & 0xF) == 0) {
 			captureIncomingBytes();
 		}
